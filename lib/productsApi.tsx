d3c07d8747cdda39791391bc8f2188b68778922f
ngumi22 <mwaurapngumi@gmail.com>
2024-03-19 16:24:57 +0300

    fr

--- lib/productsApi.tsx
+++ lib/productsApi.tsx
@@ -7,6 +7,13 @@ export const productsApi = createApi({
   reducerPath: "productsApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "https://backend-dskuts5pn-ngumi22.vercel.app",
+    headers: {
+      "Access-Control-Allow-Credentials": "true",
+      "Access-Control-Allow-Origin": "https://frontend-rho-sand.vercel.app",
+      "Access-Control-Allow-Methods": "GET, OPTIONS, PATCH, DELETE, POST, PUT",
+      "Access-Control-Allow-Headers":
+        "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+    },
   }),
   endpoints: (builder) => ({
     getAllProducts: builder.query({
