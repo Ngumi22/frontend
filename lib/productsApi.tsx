5db556262c44679df125ebdd999b1cfa1ba33d1a
ngumi22 <mwaurapngumi@gmail.com>
2024-03-19 13:41:14 +0300

    fr

--- lib/productsApi.tsx
+++ lib/productsApi.tsx
@@ -5,7 +5,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const productsApi = createApi({
   reducerPath: "productsApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://backend-sigma-swart.vercel.app/",
+  }),
   endpoints: (builder) => ({
     getAllProducts: builder.query({
       //get all products
