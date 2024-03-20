c6a5231e72e34f8c7323ccd684a2d9f81a07cae1
ngumi22 <mwaurapngumi@gmail.com>
2024-03-20 14:28:55 +0300

    fr

--- lib/productsApi.tsx
+++ lib/productsApi.tsx
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const productsApi = createApi({
   reducerPath: "productsApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://backend-kej7klhk0-ngumi22.vercel.app",
+    baseUrl: "https://backend-4uzr02waq-ngumi22.vercel.app",
     prepareHeaders: (headers) => {
       // Add CORS headers
       headers.set("Access-Control-Allow-Origin", "*");
