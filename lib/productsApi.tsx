30773c1bef542f68c42269a48393bba9da68e571
ngumi22 <mwaurapngumi@gmail.com>
2024-03-20 10:32:30 +0300

    fr

--- lib/productsApi.tsx
+++ lib/productsApi.tsx
@@ -6,7 +6,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const productsApi = createApi({
   reducerPath: "productsApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://backend-4o8xh7h2g-ngumi22.vercel.app",
+    baseUrl: "https://backend-a7lk0qfac-ngumi22.vercel.app",
   }),
   endpoints: (builder) => ({
     getAllProducts: builder.query({
